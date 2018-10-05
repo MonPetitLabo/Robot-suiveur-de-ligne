1ce747195f91baaa6ded9e9468b0d338429e05ac
Benjamin Lorentz <benjamin.lorentz@sword-group.com>
2018-10-05 14:09:09 +0200

    BLZ : finalisation de l'algorithme de déplacement et suivi de ligne

--- blz-moving-algo/blz-moving-algo.ino
+++ blz-moving-algo/blz-moving-algo.ino
@@ -1,3 +1,6 @@
+#include <string.h>
+#include <QTRSensors.h>
+
 #define ENABLE_MOTOR_LEFT 5
 #define DIRA_MOTOR_LEFT 3
 #define DIRB_MOTOR_LEFT 4
@@ -8,20 +11,38 @@
 
 // Input data
 #define MIN_INPUT 0
-#define MAX_INPUT 1023
-#define DEFAULT_ZERO 525
+#define MAX_INPUT 2000
+#define DEFAULT_ZERO 1000
 
 // Motor data
 #define MIN_MOTOR 0
 #define MAX_MOTOR 255
 
-#define MAX_VALUE_MOTOR 255
+
+#define MAX_VALUE_MOTOR 200 // Max value : 255
+
+#define STOP_BEFORE_TURN 0
+
+#define ZONE_MORT_POSITIF 500
+#define ZONE_MORT_NEGATIF -500
 
 const int SW_pin = 2; // digital pin connected to switch output
 const int X_pin = 0; // analog pin connected to X output
 const int Y_pin = 1; // analog pin connected to Y output
 
+const int CAPTEUR1 = 2;
+const int CAPTEUR2 = 1;
+const int CAPTEUR3 = 0;
+
+const bool ENABLE_CALIBRATION = true;
+
+QTRSensorsAnalog sensors ((unsigned char[]) {CAPTEUR1, CAPTEUR2, CAPTEUR3}, 3);
+
+
 void setup() {
+
+  Serial.begin(9600);
+    
   // put your setup code here, to run once:
   pinMode(ENABLE_MOTOR_LEFT, OUTPUT);
   pinMode(DIRA_MOTOR_LEFT, OUTPUT);
@@ -34,69 +55,116 @@ void setup() {
   pinMode(SW_pin, INPUT);
   digitalWrite(SW_pin, HIGH);
 
-  Serial.begin(9600);
+  //if(true) {
+    Serial.println("== Sensors calibration ==");
+    delay(2500);
+    for(int i =0; i < 250; i++){
+      Serial.print(i);
+      Serial.print("/");
+      Serial.println(250);
+      
+      sensors.calibrate();
+      delay(20);
+    }
+    Serial.println("== end of sensors calibration ==");
+  //}
+
+  
 
 }
+
+int oldDirection = 0;
+int newDirection = 0;
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  int speed = readSpeed();
+  oldDirection = newDirection;
+  newDirection = getDirection();
 
-  /*if (speed < 0) {
-    speed = speed * (-1);
-    analogWrite(ENABLE_MOTOR_LEFT, speed); //enable on
-    digitalWrite(DIRA_MOTOR_LEFT, HIGH); //one way
-    digitalWrite(DIRB_MOTOR_LEFT, LOW);
-    analogWrite(ENABLE_MOTOR_RIGHT, speed); //enable on
-    digitalWrite(DIRA_MOTOR_RIGHT, LOW); //one way
-    digitalWrite(DIRB_MOTOR_RIGHT, HIGH);
-  } else if (speed > 0) {
-    analogWrite(ENABLE_MOTOR_RIGHT, speed); //enable on
-    digitalWrite(DIRA_MOTOR_RIGHT, HIGH); //one way
-    digitalWrite(DIRB_MOTOR_RIGHT, LOW);
-    analogWrite(ENABLE_MOTOR_LEFT, speed); //enable on
-    digitalWrite(DIRA_MOTOR_LEFT, LOW); //one way
-    digitalWrite(DIRB_MOTOR_LEFT, HIGH);
-  } else { // speed = 0
-    analogWrite(ENABLE_MOTOR_LEFT, MAX_VALUE_MOTOR); //enable on
-    digitalWrite(DIRA_MOTOR_LEFT, HIGH); //one way
-    digitalWrite(DIRB_MOTOR_LEFT, LOW);
-    analogWrite(ENABLE_MOTOR_RIGHT, MAX_VALUE_MOTOR); //enable on
-    digitalWrite(DIRA_MOTOR_RIGHT, HIGH); //one way
-    digitalWrite(DIRB_MOTOR_RIGHT, LOW);
-  }*/
+  Serial.print("Direction:");
+  Serial.print(newDirection);
 
-    analogWrite(ENABLE_MOTOR_LEFT, MAX_VALUE_MOTOR); //enable on
-    digitalWrite(DIRA_MOTOR_LEFT, HIGH); //one way
-    digitalWrite(DIRB_MOTOR_LEFT, LOW);
-    analogWrite(ENABLE_MOTOR_RIGHT, MAX_VALUE_MOTOR); //enable on
-    digitalWrite(DIRA_MOTOR_RIGHT, HIGH); //one way
-    digitalWrite(DIRB_MOTOR_RIGHT, LOW);
-
-  delay(500);
-
+  if(newDirection >= ZONE_MORT_NEGATIF && 
+    newDirection <= ZONE_MORT_POSITIF) {
+      Serial.print("\t --> No turn"); 
+      goStraight();
+  } else {
+    Serial.print("\t --> Turn required"); 
+    if(newDirection <= ZONE_MORT_NEGATIF) {
+      Serial.print("\t --> turn right");
+      turnRight(needFastTurn(newDirection, oldDirection));
+    } else if (newDirection >= ZONE_MORT_POSITIF) {
+      Serial.print("\t --> turn left");
+      turnLeft(needFastTurn(newDirection, oldDirection));
+    }     
+    delay(15);
+    stop();
+  }
 }
 
-int readSpeed() {
-  int currentXAxis = analogRead(X_pin); // Range 0 - 1023
-  Serial.print("Angle joystick:");
-  Serial.print(currentXAxis);
-  Serial.print("\t");
-  int speed = currentXAxis - DEFAULT_ZERO;
-  speed = speed / (MAX_INPUT/(2 * MAX_MOTOR));
+boolean needFastTurn(int currentDirection, int previousDirection) {
+  return true;
+}
 
-  if (speed > MAX_VALUE_MOTOR) {
-    speed = MAX_VALUE_MOTOR; // Max value of motor
-  }
+void stop() {
+    analogWrite(ENABLE_MOTOR_LEFT, 0); //enable on
+    analogWrite(ENABLE_MOTOR_RIGHT, 0); //enable on
+}
 
-  if (speed < -MAX_VALUE_MOTOR) {
-    speed = -MAX_VALUE_MOTOR;
-  }
+void turnLeft(boolean isFastTurn) {
+    analogWrite(ENABLE_MOTOR_LEFT, MAX_VALUE_MOTOR); //enable on
+    digitalWrite(DIRA_MOTOR_LEFT, HIGH); //one way
+    digitalWrite(DIRB_MOTOR_LEFT, LOW);
+    if(isFastTurn) {
+      analogWrite(ENABLE_MOTOR_RIGHT, MAX_VALUE_MOTOR); //enable on
+    } else {
+      analogWrite(ENABLE_MOTOR_RIGHT, 0); //enable on
+    }
+    digitalWrite(DIRA_MOTOR_RIGHT, LOW); //one way
+    digitalWrite(DIRB_MOTOR_RIGHT, HIGH);
+    Serial.println("\tTurn Left");
+}
 
-  Serial.print(speed);
-  Serial.print("\n");
+void turnRight(boolean isFastTurn) {
+    analogWrite(ENABLE_MOTOR_RIGHT, MAX_VALUE_MOTOR); //enable on
+    digitalWrite(DIRA_MOTOR_RIGHT, HIGH); //one way
+    digitalWrite(DIRB_MOTOR_RIGHT, LOW);
 
-  return speed;
+    if(isFastTurn) {
+      analogWrite(ENABLE_MOTOR_LEFT, MAX_VALUE_MOTOR); //enable on
+    } else {
+      analogWrite(ENABLE_MOTOR_LEFT, 0); //enable on
+    }
+    digitalWrite(DIRA_MOTOR_LEFT, LOW); //one way
+    digitalWrite(DIRB_MOTOR_LEFT, HIGH);
+    Serial.println("\tTurn Right");
+}
+
+void goStraight() {
+    analogWrite(ENABLE_MOTOR_LEFT, MAX_VALUE_MOTOR); //enable on
+    digitalWrite(DIRA_MOTOR_LEFT, HIGH); //one way
+    digitalWrite(DIRB_MOTOR_LEFT, LOW);
+    analogWrite(ENABLE_MOTOR_RIGHT, MAX_VALUE_MOTOR); //enable on
+    digitalWrite(DIRA_MOTOR_RIGHT, HIGH); //one way
+    digitalWrite(DIRB_MOTOR_RIGHT, LOW);
+    Serial.println("Tout droit");
+}
+
+int getDirection() {
+  int currentXAxis = readCapteur(); // Range -1000 - 1000 
+
+  return currentXAxis;
+}
+
+int readCapteur() {
+  unsigned int sensorsValues[3];
+  int position = sensors.readLine(sensorsValues );
+  position = position - 1000;
+
+  Serial.print("Position : ");
+  Serial.println(position);
+
+  return position;
 }
 
