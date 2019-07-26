0a8d32f7dbfa357424b6f19654b5dbd60871cfe9
Sylvain LEFEVRE <sylvain-lefevre@live.fr>
2019-07-26 13:18:12 +0200

    [SLE] - Ajout du mdoule bluetooth

--- le_robot_violet/le_robot_violet.ino
+++ le_robot_violet/le_robot_violet.ino
@@ -39,8 +39,15 @@ QTRSensorsAnalog qtr((char[]) {0, 1, 2}, NUM_SENSORS);
 IRrecv irrecv(RECEIVER);
 decode_results results;
 
-//LCD
-LiquidCrystal lcd(1, 2, 10, 11, 12, 13);
+//////////////////////////////////////
+// BLUETOOTH
+/////////////////////////////////////
+const int BLUETOOTH_RX = 1; // TX3
+const int BLUETOOTH_TX = 0; // RX3
+const int BLUETOOTH_KEY = 2; // KEY
+const int BLUETOOTH_STATE = 9; // STATE
+const int AT_COMMAND_WAIT_TIME = 250; // time in ms to wait after command printed
+
 
 bool manualMode = false;
 bool start = true;
@@ -60,8 +67,20 @@ void setup() {
   pinMode(ENABLE_LEFT,OUTPUT);
   pinMode(DIRA_LEFT,OUTPUT);
   pinMode(DIRB_LEFT,OUTPUT);
-  //Serial.begin(9600);
-  lcd.begin(16, 2);
+
+  pinMode(BLUETOOTH_STATE,INPUT);
+
+  // Bluetooth pins
+  pinMode(BLUETOOTH_KEY, OUTPUT);
+  digitalWrite(BLUETOOTH_KEY, LOW);
+  Serial.begin(9600);
+
+  runCommand("AT+NAME=FABLAB", true);
+  runCommand("AT+NAME", true);
+  runCommand("AT+PSWD=2018", true);
+  runCommand("AT+PSWD?", true);
+
+  
   calibrate();
   irrecv.enableIRIn();
 }
@@ -70,34 +89,33 @@ void loop() {
   //translateIR();
   
   int position = qtr.readLine(sensors,QTR_EMITTERS_ON);
+  if(digitalRead(BLUETOOTH_STATE)== HIGH){
+    runCommand("Bonjour bluetooth", false);
+  }
+  
   if(start){
      if(manualMode){
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Mode manuel");
-        lcd.setCursor(0, 1);
+        
         manageManualMode();
       }else{
-       // displayPosition(position);
         getVitesseFromPosition(position);
       }
   }else{
-    displayPosition(position);
     rouler(0);
   }
 }
 
 void calibrate() {
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Calibration");
-  lcd.setCursor(0, 1);
-  lcd.print("dans 4s");
+//  lcd.clear();
+//  lcd.setCursor(0, 0);
+//  lcd.print("Calibration");
+//  lcd.setCursor(0, 1);
+//  lcd.print("dans 4s");
   delay(4000);
-  lcd.clear();
-  lcd.print("Debut");
-  lcd.setCursor(0, 1);
-  lcd.print("calibration");
+//  lcd.clear();
+//  lcd.print("Debut");
+//  lcd.setCursor(0, 1);
+//  lcd.print("calibration");
   //Serial.println("calibration");
   int i;
   for (i = 0; i < 500; i++)  // make the calibration take about 5 seconds
@@ -106,11 +124,11 @@ void calibrate() {
     delay(20);
   }
   //Serial.println("fin calibration");
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Calibration");
-  lcd.setCursor(0, 1);
-  lcd.print("terminee");
+//  lcd.clear();
+//  lcd.setCursor(0, 0);
+//  lcd.print("Calibration");
+//  lcd.setCursor(0, 1);
+//  lcd.print("terminee");
   delay(1000);
 }
 
@@ -149,19 +167,19 @@ void actionnerMoteur2(int vitesse) {
   }
 }
 
-void displayPosition(int position) {
-  affichageTime++;
-  if(affichageTime==255){
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("1:" +String(sensors[0]));
-    lcd.setCursor(0, 1);
-    lcd.print("2:" +String(sensors[1]));
-    lcd.setCursor(8, 1);
-    lcd.print("3:" +String(sensors[2]));
-    affichageTime=0;
-  }
-}
+//void displayPosition(int position) {
+//  affichageTime++;
+//  if(affichageTime==255){
+//    lcd.clear();
+//    lcd.setCursor(0, 0);
+//    lcd.print("1:" +String(sensors[0]));
+//    lcd.setCursor(0, 1);
+//    lcd.print("2:" +String(sensors[1]));
+//    lcd.setCursor(8, 1);
+//    lcd.print("3:" +String(sensors[2]));
+//    affichageTime=0;
+//  }
+//}
 
 void manageManualMode() {
     if(manualStart){
@@ -169,34 +187,34 @@ void manageManualMode() {
         rouler(manuelSpeed);
         float pourcentageVitesse = float(manuelSpeed)/maxSpeed*100;
         String vitesse = String(pourcentageVitesse) + "%";
-        lcd.print("Avance : " + vitesse);
+//        lcd.print("Avance : " + vitesse);
       }else if(manualDirection=="r"){
         rouler(manuelSpeed*-1);
         float pourcentageVitesse = float(manuelSpeed)/maxSpeed*100;
         String vitesse = String(pourcentageVitesse) + "%";
-        lcd.print("Recule : "+ vitesse);
+//        lcd.print("Recule : "+ vitesse);
       }else if(manualDirection=="d1"){
         getVitesseFromPosition(750);
-        lcd.print("Virage droite 1");
+  //      lcd.print("Virage droite 1");
       }else if(manualDirection=="d2"){
         getVitesseFromPosition(500);
-        lcd.print("Virage droite 2");
+  //      lcd.print("Virage droite 2");
       }else if(manualDirection=="d3"){
         getVitesseFromPosition(0);
-        lcd.print("Virage droite 3");
+  //      lcd.print("Virage droite 3");
       }else if(manualDirection=="g1"){
         getVitesseFromPosition(1250);
-        lcd.print("Virage gauche 1");
+  //      lcd.print("Virage gauche 1");
       }else if(manualDirection=="g2"){
         getVitesseFromPosition(1500);
-        lcd.print("Virage gauche 2");
+  //      lcd.print("Virage gauche 2");
       }else if(manualDirection=="g3"){
         getVitesseFromPosition(2000);
-        lcd.print("Virage gauche 3");
+  //      lcd.print("Virage gauche 3");
       }
     }else{
       rouler(0);
-      lcd.print("Pause");
+  //    lcd.print("Pause");
     }
 }
 
@@ -225,33 +243,33 @@ void getVitesseFromPosition(int position){
 
 void translateIR(){
   if(irrecv.decode(&results)) {
-    lcd.clear();
-    lcd.print("Commande");
-    lcd.setCursor(0, 1);
+//    lcd.clear();
+//    lcd.print("Commande");
+//    lcd.setCursor(0, 1);
     switch(results.value) {
       case 0xFFA25D:
         start = !start;
-        lcd.clear();
-        lcd.setCursor(0, 0);
+//        lcd.clear();
+//        lcd.setCursor(0, 0);
         if(start){
-          lcd.print("Start");
+     //     lcd.print("Start");
         }else{
-          lcd.print("Stop");
+     //     lcd.print("Stop");
         }
-        lcd.setCursor(0, 1);
-        lcd.print("Robot");
+     //   lcd.setCursor(0, 1);
+     //   lcd.print("Robot");
         break;
       case 0xFFE21D:
         manualMode = !manualMode;
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Change mode");
+//        lcd.clear();
+//        lcd.setCursor(0, 0);
+//        lcd.print("Change mode");
         break;
       case 0xFF629D: 
         manualDirection = "a";
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Avancer");
+//        lcd.clear();
+//        lcd.setCursor(0, 0);
+//        lcd.print("Avancer");
         break;
       case 0xFF22DD:
         if(manualDirection == "g1"){
@@ -261,20 +279,20 @@ void translateIR(){
         }else{
           manualDirection = "g1";
         }
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Braquage gauche");
+//        lcd.clear();
+//        lcd.setCursor(0, 0);
+//        lcd.print("Braquage gauche");
       
         break;
       case 0xFF02FD:
         manualStart = !manualStart;
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        if(manualStart){
-          lcd.print("Resume robot");
-        }else{
-          lcd.print("Mise en pause");
-        }
+//        lcd.clear();
+//        lcd.setCursor(0, 0);
+//        if(manualStart){
+//          lcd.print("Resume robot");
+//        }else{
+//          lcd.print("Mise en pause");
+//        }
         
         break;
       case 0xFFC23D:
@@ -285,9 +303,9 @@ void translateIR(){
         }else{
           manualDirection = "d1";
         }
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Braquage droite");
+//        lcd.clear();
+//        lcd.setCursor(0, 0);
+//        lcd.print("Braquage droite");
       
         break;
       case 0xFFE01F:
@@ -296,15 +314,15 @@ void translateIR(){
         }else{
           manuelSpeed = minSpeed;
         }
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Freiner");
+//        lcd.clear();
+//        lcd.setCursor(0, 0);
+//        lcd.print("Freiner");
         break;
       case 0xFFA857:
         manualDirection = "r";
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Reculer");
+//        lcd.clear();
+//        lcd.setCursor(0, 0);
+//        lcd.print("Reculer");
         break;
       case 0xFF906F:
         if(manuelSpeed <= maxSpeed - 10){
@@ -312,31 +330,65 @@ void translateIR(){
         }else{
           manuelSpeed = maxSpeed;
         }
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Accelerer");
+//        lcd.clear();
+//        lcd.setCursor(0, 0);
+//        lcd.print("Accelerer");
         break;
       case 0xFF9867:
         start = false;
         calibrate();
         break;
-      case 0xFFB04F: lcd.print("ST/REPT");    break;
-      case 0xFF6897: lcd.print("0");    break;
-      case 0xFF30CF: lcd.print("1");    break;
-      case 0xFF18E7: lcd.print("2");    break;
-      case 0xFF7A85: lcd.print("3");    break;
-      case 0xFF10EF: lcd.print("4");    break;
-      case 0xFF38C7: lcd.print("5");    break;
-      case 0xFF5AA5: lcd.print("6");    break;
-      case 0xFF42BD: lcd.print("7");    break;
-      case 0xFF4AB5: lcd.print("8");    break;
-      case 0xFF52AD: lcd.print("9");    break;
-      case 0xFFFFFFFF: lcd.print(" REPEAT");break;  
-      default: 
-        lcd.print(" other button   ");
+//      case 0xFFB04F: lcd.print("ST/REPT");    break;
+//      case 0xFF6897: lcd.print("0");    break;
+//      case 0xFF30CF: lcd.print("1");    break;
+//      case 0xFF18E7: lcd.print("2");    break;
+//      case 0xFF7A85: lcd.print("3");    break;
+//      case 0xFF10EF: lcd.print("4");    break;
+//      case 0xFF38C7: lcd.print("5");    break;
+//      case 0xFF5AA5: lcd.print("6");    break;
+//      case 0xFF42BD: lcd.print("7");    break;
+//      case 0xFF4AB5: lcd.print("8");    break;
+//      case 0xFF52AD: lcd.print("9");    break;
+//      case 0xFFFFFFFF: lcd.print(" REPEAT");break;  
+//      default: 
+//        lcd.print(" other button   ");
     }
     delay(500);
     irrecv.resume();
   }
   
+}
+
+
+/**
+ * Send a commond to the bluetooth module
+ * 
+ * command : command to send
+ * isATCommand : specify if the command is for the module configuration or is to send to connected devices
+ */
+void runCommand(String command, boolean isATCommand){
+  if(command.length() > 0){
+
+    // activate command mode
+    if(isATCommand){
+      digitalWrite(BLUETOOTH_KEY, HIGH);
+    }
+
+    // print command
+    Serial.print(command);
+    Serial.print("\r\n");
+
+    // wait for answer
+    delay(300);
+
+    // get and print answer
+    if(Serial.available()){
+      String input = Serial.readString();
+    }
+
+    // unactive command mode
+    if(isATCommand){
+      digitalWrite(BLUETOOTH_KEY, LOW);
+    }
+  }
 }
